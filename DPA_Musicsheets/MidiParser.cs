d1c604e19c6c6002fe37bc9004b8944c5b3d95c0
jorgdebont <jacbont@student.avans.nl>
2016-09-09 15:35:58 +0200

    Started on creating notes, printen to stdout, finish factory and send to factory

--- DPA_Musicsheets/MidiParser.cs
+++ DPA_Musicsheets/MidiParser.cs
@@ -20,10 +20,14 @@ namespace DPA_Musicsheets
         private static D_Staff parseSequence(Sequence sequence)
         {
             D_Staff staff = new D_Staff(0,0);
-            
+            Tuple<int, int> measure = new Tuple<int, int>(-1, -1);
 
             
             Track track = sequence[1];
+            int keycode_on = -1;
+            int delta_time_on; // plz kill me
+
+            int delta_time_off; // plz kill me
 
             foreach (var midiEvent in track.Iterator())
             {
@@ -35,12 +39,30 @@ namespace DPA_Musicsheets
                         var channelMessage = midiEvent.MidiMessage as ChannelMessage;
                         // Data1: De keycode. 0 = laagste C, 1 = laagste C#, 2 = laagste D etc.
                         // 160 is centrale C op piano.
-                        Console.WriteLine(
-                            String.Format(
-                                "Keycode: {0}, Command: {1}, absolute time: {2}, delta time: {3}"
-                                , channelMessage.Data1, channelMessage.Command, midiEvent.AbsoluteTicks, midiEvent.DeltaTicks
-                            )
-                        );
+
+                        if (channelMessage.Command.ToString() == "NoteOn")
+                        {
+                            keycode_on = channelMessage.Data1;
+                            delta_time_off = midiEvent.DeltaTicks;
+                            Console.WriteLine(String.Format("note: none, length: {0}", delta_time_off));
+                        }
+                        else if (channelMessage.Command.ToString() == "NoteOff")
+                        {
+                            delta_time_on = midiEvent.DeltaTicks;
+                            Console.WriteLine(String.Format("note: {0}, length: {1}", keycode_on, delta_time_on));
+                        }
+                        else
+                        {
+                            Console.WriteLine("YOU DONE FUCKED UP!");
+                            Console.WriteLine(
+                                String.Format(
+                                    "Keycode: {0}, Command: {1}, absolute time: {2}, delta time: {3}"
+                                    , channelMessage.Data1, channelMessage.Command, midiEvent.AbsoluteTicks, midiEvent.DeltaTicks
+                                )
+                            );
+                        }
+
+
 
                         break;
                     case MessageType.Meta:
@@ -49,7 +71,9 @@ namespace DPA_Musicsheets
                         if (metaMessage.MetaType == MetaType.TimeSignature) {
                             int top_number = meta_bytes[0];
                             int bottom_number = (int) (1 / Math.Pow(meta_bytes[1], -2));
-                            Console.WriteLine(String.Format("Time signature is {0}/{1}", top_number, bottom_number));
+                            measure = new Tuple<int, int>(top_number, bottom_number);
+                            //TODO: MEASURE MIGHT NOT BE FILLED ON FIRST COMMAND 
+                            Console.WriteLine(String.Format("Time signature is {0}/{1}", measure.Item1, measure.Item2));
                         }
                         break;
                     default:
@@ -57,6 +81,7 @@ namespace DPA_Musicsheets
                 }
                 
             }
+            Console.WriteLine(String.Format("{0}/{1}", measure.Item1, measure.Item2));
             return staff;
         }
 
