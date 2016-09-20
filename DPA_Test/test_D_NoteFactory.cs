291108eff36c34758293dc3f0ebe4e63949a5208
ThomasvLingen <thovali96@gmail.com>
2016-09-20 12:58:12 +0200

    test: add unittest project

---
+++ DPA_Test/test_D_NoteFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DPA_Test {
+    [Category("D_NoteFactory")]
+    public class test_D_NoteFactory {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
