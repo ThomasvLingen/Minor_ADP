8c00602daf9a3dbc00a8ba1989e3915ec77e2e4b
ThomasVanLingen <thovali96@gmail.com>
2016-10-01 19:07:26 +0200

    feat: tokenise lilypond input

--- DPA_Musicsheets/LilypondParser.cs
+++ DPA_Musicsheets/LilypondParser.cs
@@ -22,8 +22,26 @@ namespace DPA_Musicsheets {
             D_Staff staff = new D_Staff();
 
             List<string> file_lines = new List<string>(System.IO.File.ReadAllLines(lilypondFilePath));
+            List<string> tokens = this.getLilypondTokens(file_lines);
 
             return staff;
+        }
+
+        private List<string> getLilypondTokens(List<string> file_lines)
+        {
+            List<string> tokens = new List<string>();
+
+            foreach(string line in file_lines) {
+                string[] line_tokens = line.Split(' ');
+
+                foreach (string token in line_tokens) {
+                    if (token != "") {
+                        tokens.Add(token);
+                    }
+                }
+            }
+
+            return tokens;
         }
     }
 }
