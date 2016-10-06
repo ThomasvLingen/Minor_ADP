5ce49f104fe700e7646566d9e54971838807dcb8
ThomasVanLingen <thovali96@gmail.com>
2016-10-06 22:24:00 +0200

    refactor: clean up midiParser

--- DPA_Musicsheets/MidiParser.cs
+++ DPA_Musicsheets/MidiParser.cs
@@ -28,25 +28,52 @@ namespace DPA_Musicsheets
             return parseSequence(sequence);
         }
 
-        private static void set_tempo(Sequence sequence, D_Staff staff)
+        private static List<Tuple<MidiEvent, MetaMessage>> getMetaMessages(Track track)
         {
-            Track track = sequence[0];
+            List<Tuple<MidiEvent, MetaMessage>> messages = new List<Tuple<MidiEvent, MetaMessage>>();
 
             foreach (var midiEvent in track.Iterator()) {
-                // Elke messagetype komt ook overeen met een class. Daarom moet elke keer gecast worden.
-                switch (midiEvent.MidiMessage.MessageType) {
-                    case MessageType.Meta:
-                        var metaMessage = midiEvent.MidiMessage as MetaMessage;
-                        byte[] meta_bytes = metaMessage.GetBytes();
+                if (midiEvent.MidiMessage.MessageType == MessageType.Meta) {
+                    messages.Add(
+                        new Tuple<MidiEvent, MetaMessage>(midiEvent, midiEvent.MidiMessage as MetaMessage)
+                    );
+                }
+            }
 
-                        // BPM
-                        if (metaMessage.MetaType == MetaType.Tempo) {
-                            // Bitshifting is nodig om het tempo in BPM te berekenen
-                            int tempo = (meta_bytes[0] & 0xff) << 16 | (meta_bytes[1] & 0xff) << 8 | (meta_bytes[2] & 0xff);
-                            staff.tempo = 60000000 / tempo;
-                            return;
-                        }
-                        break;
+            return messages;
+        }
+
+        private static List<Tuple<MidiEvent, ChannelMessage>> getChannelMessages(Track track)
+        {
+            List<Tuple<MidiEvent, ChannelMessage>> messages = new List<Tuple<MidiEvent, ChannelMessage>>();
+
+            foreach (var midiEvent in track.Iterator()) {
+                if (midiEvent.MidiMessage.MessageType == MessageType.Channel) {
+                    messages.Add(
+                        new Tuple<MidiEvent, ChannelMessage>(midiEvent, midiEvent.MidiMessage as ChannelMessage)
+                    );
+                }
+            }
+
+            return messages;
+        }
+
+        private static void set_tempo(Sequence sequence, D_Staff staff)
+        {
+            // Tempo message is in channel 0
+            List<Tuple<MidiEvent, MetaMessage>> meta_messages = getMetaMessages(sequence[0]);
+
+            foreach (Tuple<MidiEvent, MetaMessage> metaPair in meta_messages) {
+                MetaMessage metaMessage = metaPair.Item2;
+
+                byte[] meta_bytes = metaPair.Item2.GetBytes();
+
+                // BPM
+                if (metaMessage.MetaType == MetaType.Tempo) {
+                    // Bitshifting is nodig om het tempo in BPM te berekenen
+                    int tempo = (meta_bytes[0] & 0xff) << 16 | (meta_bytes[1] & 0xff) << 8 | (meta_bytes[2] & 0xff);
+                    staff.tempo = 60000000 / tempo;
+                    return;
                 }
             }
 
@@ -56,28 +83,24 @@ namespace DPA_Musicsheets
         private static void set_measures(Sequence sequence, D_Staff staff)
         {
             // This function requires staff.num_of_beats to be set!
-            Track track = sequence[0];
 
-            foreach (var midiEvent in track.Iterator()) {
-                // Elke messagetype komt ook overeen met een class. Daarom moet elke keer gecast worden.
-                switch (midiEvent.MidiMessage.MessageType) {
-                    case MessageType.Meta:
-                        var metaMessage = midiEvent.MidiMessage as MetaMessage;
-                        byte[] meta_bytes = metaMessage.GetBytes();
+            // TimeSignature messages are in channel 0
+            List<Tuple<MidiEvent, MetaMessage>> meta_messages = getMetaMessages(sequence[0]);
 
-                        // Time signature
-                        if (metaMessage.MetaType == MetaType.TimeSignature) {
-                            int top_number = meta_bytes[0];
-                            int bottom_number = (int)(Math.Pow(2, meta_bytes[1]));
-                            int start_beat = midiEvent.AbsoluteTicks / sequence.Division;
+            foreach (Tuple<MidiEvent, MetaMessage> metaPair in meta_messages) {
+                MidiEvent midiEvent = metaPair.Item1;
+                MetaMessage metaMessage = metaPair.Item2;
 
-                            staff.addMeasure(top_number, bottom_number, start_beat);
-                            
-                        }
+                byte[] meta_bytes = metaMessage.GetBytes();
 
-                        break;
+                // Time signature
+                if (metaMessage.MetaType == MetaType.TimeSignature) {
+                    int top_number = meta_bytes[0];
+                    int bottom_number = (int)(Math.Pow(2, meta_bytes[1]));
+                    int start_beat = midiEvent.AbsoluteTicks / sequence.Division;
+
+                    staff.addMeasure(top_number, bottom_number, start_beat);
                 }
-
             }
 
             staff.setMeasureEndTimes();
@@ -85,24 +108,21 @@ namespace DPA_Musicsheets
 
         private static void set_num_of_beats(Sequence sequence, D_Staff staff)
         {
-            Track track = sequence[1];
+            // TimeSignature messages are in channel 0
+            List<Tuple<MidiEvent, MetaMessage>> meta_messages = getMetaMessages(sequence[1]);
 
-            foreach (var midiEvent in track.Iterator()) {
-                switch (midiEvent.MidiMessage.MessageType) {
-                    case MessageType.Meta:
-                        var metaMessage = midiEvent.MidiMessage as MetaMessage;
-                        byte[] meta_bytes = metaMessage.GetBytes();
+            foreach (Tuple<MidiEvent, MetaMessage> metaPair in meta_messages) {
+                MidiEvent midiEvent = metaPair.Item1;
+                MetaMessage metaMessage = metaPair.Item2;
 
-                        // EndOfTrack
-                        if (metaMessage.MetaType == MetaType.EndOfTrack) {
-                            int beats = midiEvent.AbsoluteTicks / sequence.Division;
+                byte[] meta_bytes = metaMessage.GetBytes();
 
-                            staff.num_of_beats = beats;
-                        }
+                // EndOfTrack
+                if (metaMessage.MetaType == MetaType.EndOfTrack) {
+                    int beats = midiEvent.AbsoluteTicks / sequence.Division;
 
-                        break;
+                    staff.num_of_beats = beats;
                 }
-
             }
         }
 
@@ -121,46 +141,49 @@ namespace DPA_Musicsheets
         private static List<D_Note> get_notes_from_track(Track track, D_Staff staff, int ticks_per_beat)
         {
             List<D_Note> notes = new List<D_Note>();
-            int note_count = 0;
 
             MidiEvent previous_midi_event = null;
-            foreach (var midiEvent in track.Iterator()) {
-                switch (midiEvent.MidiMessage.MessageType) {
-                    case MessageType.Channel:
-                        var channelMessage = midiEvent.MidiMessage as ChannelMessage;
 
-                        if (previous_midi_event != null) {
-                            if (channelMessage.Command.ToString() == "NoteOn" && channelMessage.Data2 == 0) {
-                                // Make note
-                                note_count++;
+            List<Tuple<MidiEvent, ChannelMessage>> channel_messages = getChannelMessages(track);
 
-                                double note_beats = ((double)midiEvent.AbsoluteTicks - (double)previous_midi_event.AbsoluteTicks) / (double)ticks_per_beat;
-                                int note_beats_measured = (int)(note_beats * 4);
+            foreach(Tuple<MidiEvent, ChannelMessage> channel_pair in channel_messages) {
+                MidiEvent midiEvent = channel_pair.Item1;
+                ChannelMessage channelMessage = channel_pair.Item2;
 
-                                notes.Add(D_NoteFactory.create_note(channelMessage.Data1, note_beats_measured));
-                            }
-                            else if (channelMessage.Command.ToString() == "NoteOn" && channelMessage.Data2 == 90) {
-                                // Make rest
-                                if (midiEvent.DeltaTicks > 0) {
-                                    double note_beats = (double)midiEvent.DeltaTicks / (double)ticks_per_beat;
-                                    int note_beats_measured = (int)(note_beats * 4);
-                                    notes.Add(D_NoteFactory.create_rest(note_beats_measured));
-                                }
-                            }
-                        }
+                if (previous_midi_event != null) {
+                    D_Note note = get_note(midiEvent, channelMessage, previous_midi_event, ticks_per_beat);
 
-                        previous_midi_event = midiEvent;
-                    break;
+                    if (note != null) {
+                        notes.Add(get_note(midiEvent, channelMessage, previous_midi_event, ticks_per_beat));
+                    }
                 }
-            }
 
-            int total_beats = 0;
-
-            foreach (D_Note note in notes) {
-                total_beats += note.length;
+                previous_midi_event = midiEvent;
             }
 
             return notes;
+        }
+
+        private static D_Note get_note(MidiEvent midiEvent, ChannelMessage channelMessage, MidiEvent previous_midi_event, int ticks_per_beat)
+        {
+            if (channelMessage.Command.ToString() == "NoteOn" && channelMessage.Data2 == 0) {
+                // Make note
+
+                double note_beats = ((double)midiEvent.AbsoluteTicks - (double)previous_midi_event.AbsoluteTicks) / (double)ticks_per_beat;
+                int note_beats_measured = (int)(note_beats * 4);
+
+                return D_NoteFactory.create_note(channelMessage.Data1, note_beats_measured);
+            }
+            else if (channelMessage.Command.ToString() == "NoteOn" && channelMessage.Data2 == 90) {
+                // Make rest
+                if (midiEvent.DeltaTicks > 0) {
+                    double note_beats = (double)midiEvent.DeltaTicks / (double)ticks_per_beat;
+                    int note_beats_measured = (int)(note_beats * 4);
+                    return D_NoteFactory.create_rest(note_beats_measured);
+                }
+            }
+
+            return null;
         }
 
         private static D_Staff parseSequence(Sequence sequence)
