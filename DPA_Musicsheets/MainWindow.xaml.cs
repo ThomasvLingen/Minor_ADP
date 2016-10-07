1e50bd20762a26a9406975e31d49de9a2a7c462b
jorgdebont <jacbont@student.avans.nl>
2016-10-07 15:51:02 +0200

    refactor: Cleaned up useless imports

--- DPA_Musicsheets/MainWindow.xaml.cs
+++ DPA_Musicsheets/MainWindow.xaml.cs
@@ -4,20 +4,7 @@ using PSAMWPFControlLibrary;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DPA_Musicsheets
 {
