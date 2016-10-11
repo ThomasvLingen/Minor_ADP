72670c87e5b95b21a37e5179b075a65113e199ae
ThomasvLingen <thovali96@gmail.com>
2016-10-11 17:26:33 +0200

    test: fix D_NoteFactory tests
    The interface of D_NoteFactory.create_note changed,
    so tests had to be changed accordingly

--- DPA_Test/test_D_NoteFactory.cs
+++ DPA_Test/test_D_NoteFactory.cs
@@ -6,39 +6,19 @@ namespace DPA_Test {
     [Category("D_NoteFactory")]
     public class test_D_NoteFactory {
         [Test]
-        public void TestMethod1()
-        {
-            Assert.IsTrue(true);
-        }
-
-        [Test]
         public void Test_create_notes()
         {
             D_Note test_note;
 
-            // Central C (c4)
-            test_note = D_NoteFactory.create_note(60, 10);
+            test_note = D_NoteFactory.create_note(NoteLevel.C, NoteAlteration.sharp, 4, 4);
             Assert.AreEqual(NoteLevel.C, test_note.level);
-            Assert.AreEqual(4, test_note.octave);
-            Assert.AreEqual(NoteAlteration.none, test_note.alteration);
-
-            // Central F# (f#4)
-            test_note = D_NoteFactory.create_note(66, 10);
-            Assert.AreEqual(NoteLevel.F, test_note.level);
-            Assert.AreEqual(4, test_note.octave);
+            Assert.AreEqual(false, test_note.is_rest);
+            Assert.AreEqual(4, test_note.length);
+            Assert.AreEqual(false, test_note.length_modifier);
             Assert.AreEqual(NoteAlteration.sharp, test_note.alteration);
 
-            // High Bb (Bb5)
-            test_note = D_NoteFactory.create_note(82, 10);
-            Assert.AreEqual(NoteLevel.B, test_note.level);
-            Assert.AreEqual(5, test_note.octave);
-            Assert.AreEqual(NoteAlteration.flat, test_note.alteration);
-
-            // Low Eb (Eb3)
-            test_note = D_NoteFactory.create_note(51, 10);
-            Assert.AreEqual(NoteLevel.E, test_note.level);
-            Assert.AreEqual(3, test_note.octave);
-            Assert.AreEqual(NoteAlteration.flat, test_note.alteration);
+            test_note = D_NoteFactory.create_note(NoteLevel.C, NoteAlteration.sharp, 4, 6);
+            Assert.AreEqual(true, test_note.length_modifier);
         }
     }
 }
