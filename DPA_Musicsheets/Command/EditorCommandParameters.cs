d1712e60ce27e6eb950c9de4a695d692b0717c7a
ThomasvLingen <thovali96@gmail.com>
2016-10-13 13:49:34 +0200

    fix: add missing file
    This was supposed to be committed in the previous commit

---
+++ DPA_Musicsheets/Command/EditorCommandParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Command {
+    public class EditorCommandParameters {
+        public string current_selection { get; set; }
+        public int current_selection_index { get; set; }
+
+        public EditorCommandParameters(string current_selection, int current_selection_index)
+        {
+            this.current_selection = current_selection;
+            this.current_selection_index = current_selection_index;
+        }
+    }
+}
