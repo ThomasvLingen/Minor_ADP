88a9252aca5929511ba4a9e73e01251eee4286ab
ThomasvLingen <thovali96@gmail.com>
2016-10-14 13:18:57 +0200

    feat: add save to pdf hotkey

---
+++ DPA_Musicsheets/ChainOfResp/SavePDFHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DPA_Musicsheets.Command;
+
+namespace DPA_Musicsheets.ChainOfResp {
+    public class SavePDFHandler : HotkeyHandler {
+        private List<Key> hotkey = new List<Key>() {
+            Key.LeftCtrl,
+            Key.P
+        };
+
+        protected override bool shouldHandle(List<Key> keys_down)
+        {
+            return keys_down.ContainsSameItems(this.hotkey);
+        }
+
+        protected override void handle(EditorCommandList commands)
+        {
+            commands.executeCommand("save_file_pdf");
+        }
+    }
+}
---
+++ DPA_Musicsheets/Command/SavePDFCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Command {
+    public class SavePDFCommand : EditorCommand {
+        public string name { get; }
+
+        public SavePDFCommand()
+        {
+            this.name = "save_file_pdf";
+        }
+
+        public void execute(Editor editor, EditorCommandParameters parameters)
+        {
+            editor.saveToPDF();
+        }
+    }
+}
